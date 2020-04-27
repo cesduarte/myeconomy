590c8242559939da244c57745b54c041f7e2bdf9
cesduarte <59450791+cesduarte@users.noreply.github.com>
2020-04-27 18:28:30 -0300

    Atualização banco

--- Banco/Scripts/Usuarios/Procedure_InserirUsuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Procedure_InserirUsuarios`(IN Descricao nvarchar(200),
-IN Senha nvarchar(200),
-IN Email nvarchar(200),
-IN Isdelete bool,
-IN TrocarSenha bool)
-BEGIN
-insert into tbl_usuarios(Descricao,Senha,Email,Isdelete, TrocarSenha) values(Descricao, Senha, Email, Isdelete, TrocarSenha);
-END
--- Banco/Scripts/Usuarios/Procedure_PesquisaUsuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `Procedure_PesquisaUsuarios`(in _descricao varchar(100), in _isdelete bool)
-BEGIN
-if(_descricao ='') then
-select * from tbl_usuarios where Isdelete = _isdelete;
-else
-select * from tbl_usuarios where Descricao = _descricao and Isdelete = _isdelete;
-end if;
-END
