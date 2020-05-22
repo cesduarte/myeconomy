f168fa20fcb6567311393127348e41f06cf595b9
Carlos Duarte <carlosemanuel@demaria.com.br>
2020-05-22 17:21:25 -0300

    Atualização

--- MyEconomy/Banco/Scripts/1.0/ContasBancárias/Procedure_PesquisaContasBancarias.sql
+++ MyEconomy/Banco/Scripts/1.0/ContasBancárias/Procedure_PesquisaContasBancarias.sql
@@ -6,56 +6,56 @@ select
 b.idcontasbancarias,
 b.DescricaoContasBancarias,
 a.descricao as descricaousuario,
-sum(c.ValorOcorrencia)Saldo
+(select sum(c.ValorOcorrencia)from tbl_extratobancario c where c.Idcontasbancarias = b.Idcontasbancarias) as Saldo
 from
 tbl_usuarios a,
-tbl_contasbancarias b,
-tbl_extratobancario c
+tbl_contasbancarias b
+
 
 
 where
-b.idusuario = a.idusuario and  b.Isdelete = _isdelete and a.idusuario<>1 and c.Idcontasbancarias and b.idcontasbancarias;
+b.idusuario = a.idusuario and  b.Isdelete = _isdelete and a.idusuario<>1;
 elseif(_descricaocontasbancarias<>'' && _idusuario = 1) then
 
 select
 b.idcontasbancarias,
 b.DescricaoContasBancarias,
 a.descricao as descricaousuario,
-sum(c.ValorOcorrencia)Saldo
+(select sum(c.ValorOcorrencia)from tbl_extratobancario c where c.Idcontasbancarias = b.Idcontasbancarias) as Saldo
 from
 tbl_usuarios a,
 tbl_contasbancarias b
 
 
 where
-b.idusuario = a.idusuario and  b.Isdelete = false and DescricaoContasBancarias  LIKE CONCAT('%', _descricaocontasbancarias, '%') and a.idusuario<>1 and c.Idcontasbancarias and b.idcontasbancarias;
+b.idusuario = a.idusuario and  b.Isdelete = false and DescricaoContasBancarias  LIKE CONCAT('%', _descricaocontasbancarias, '%') and a.idusuario<>1;
 
 elseif(_descricaocontasbancarias='' && _idusuario <> 1) then
 select
 b.idcontasbancarias,
 b.DescricaoContasBancarias,
 a.descricao as descricaousuario,
-sum(c.ValorOcorrencia)Saldo
+(select sum(c.ValorOcorrencia)from tbl_extratobancario c where c.Idcontasbancarias = b.Idcontasbancarias) as Saldo
 from
 tbl_usuarios a,
 tbl_contasbancarias b
 
 
 where
-b.idusuario = a.idusuario and  b.Isdelete = _isdelete and  a.idusuario =_idusuario and c.Idcontasbancarias and b.idcontasbancarias ;
+b.idusuario = a.idusuario and  b.Isdelete = _isdelete and  a.idusuario =_idusuario ;
 else
 select
 b.idcontasbancarias,
 b.DescricaoContasBancarias,
 a.descricao as descricaousuario,
-sum(c.ValorOcorrencia)Saldo
+(select sum(c.ValorOcorrencia)from tbl_extratobancario c where c.Idcontasbancarias = b.Idcontasbancarias) as Saldo
 from
 tbl_usuarios a,
 tbl_contasbancarias b
 
 
 where
-b.idusuario = a.idusuario and  b.Isdelete = _isdelete and  a.idusuario =_idusuario and DescricaoContasBancarias  LIKE CONCAT('%', _descricaocontasbancarias, '%') and c.Idcontasbancarias and b.idcontasbancarias;
+b.idusuario = a.idusuario and  b.Isdelete = _isdelete and  a.idusuario =_idusuario and DescricaoContasBancarias  LIKE CONCAT('%', _descricaocontasbancarias, '%');
 
 end if;
 
